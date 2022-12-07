fd16638d70229e0c121b4315190f5d0b8ac182f8
elias@showk.me <elias@huggingface.co>
2022-12-07 15:54:39 +0100

    finish trackdechet sirene index

--- dags/trackdechets_search_sirene.py
+++ dags/trackdechets_search_sirene.py
@@ -2,6 +2,7 @@ import logging
 from pathlib import Path
 import shutil
 import subprocess
+import sys
 import tempfile
 from datetime import datetime
 from airflow.decorators import dag, task
@@ -11,11 +12,21 @@ from airflow.models import Variable, Connection
 logging.basicConfig()
 logger = logging.getLogger()
 
+es_connection = Connection.get_connection_from_secrets(
+    "trackdechets_search_sirene_elasticsearch_url"
+)
+
+es_credentials = ""
+if es_connection.login and es_connection.password:
+    es_credentials = f"{es_connection.login}:{es_connection.password}@"
+
+es_schema = "http"
+if es_connection.schema:
+    es_schema = f"{es_connection.schema}"
+
 environ = {
     "FORCE_LOGGER_CONSOLE": Variable.get("FORCE_LOGGER_CONSOLE"),
-    "ELASTICSEARCH_URL": Connection.get_connection_from_secrets(
-        "trackdechets_search_sirene_elasticsearch_url"
-    ).get_uri(),
+    "ELASTICSEARCH_URL": f"{es_schema}://{es_credentials}{es_connection.host}:{es_connection.port}",
     "DD_LOGS_ENABLED": Variable.get("DD_LOGS_ENABLED"),
     "DD_TRACE_ENABLED": Variable.get("DD_TRACE_ENABLED"),
     "DD_API_KEY": Variable.get("DD_API_KEY"),
@@ -49,21 +60,29 @@ def trackdechets_search_sirene():
         install_command = "npm install --quiet"
         completed_process = subprocess.run(
             install_command,
-            check=True,
+            check=False,
             capture_output=True,
             shell=True,
             cwd=tmp_dir / "trackdechets" / "search",
         )
-        logger.info(completed_process)
+        logger.info(completed_process.stderr)
+        logger.info(completed_process.stdout)
+        if completed_process.returncode != 0:
+            raise Exception(completed_process)
+
         build_command = "npm run build"
         completed_process = subprocess.run(
             build_command,
-            check=True,
+            check=False,
             capture_output=True,
             shell=True,
             cwd=tmp_dir / "trackdechets" / "search",
         )
-        logger.info(completed_process)
+        logger.info(completed_process.stderr)
+        logger.info(completed_process.stdout)
+        if completed_process.returncode != 0:
+            raise Exception(completed_process)
+
         return str(tmp_dir)
 
     @task
@@ -73,15 +92,24 @@ def trackdechets_search_sirene():
         """
         tmp_dir = Path(tmp_dir)
         index_command = "npm run index"
-        completed_process = subprocess.run(
+        process = subprocess.Popen(
             index_command,
-            check=True,
-            capture_output=True,
             shell=True,
             cwd=tmp_dir / "trackdechets" / "search",
             env=environ,
+            stdout=subprocess.PIPE,
         )
-        logger.info(completed_process)
+
+        while True:
+            line = process.stdout.readline()
+            if not line:
+                break
+            logging.info(line.rstrip().decode('utf-8'))
+
+        while process.wait():
+            if process.returncode != 0:
+                raise Exception(process)
+
         return str(tmp_dir)
 
     @task
